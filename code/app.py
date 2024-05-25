f1e773a507e103c2366809876615d49936283454
ulricht01 <99655536+ulricht01@users.noreply.github.com>
2024-05-25 03:14:59 +0200

    0.3.2

--- code/app.py
+++ code/app.py
@@ -18,6 +18,22 @@ config = {
 def index():
     return render_template('index.html')
 
+@app.route('/volby_1')
+def volby_1():
+    return render_template('volby_1.html')
+
+@app.route('/volby_2')
+def volby_2():
+    return render_template('volby_2.html')
+
+@app.route('/volby_3')
+def volby_3():
+    return render_template('volby_3.html')
+
+@app.route('/volby_4')
+def volby_4():
+    return render_template('volby_4.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5000)
